7d5bc45afe7f05651d858368f4d06d0c86c936d6
dhanani94 <dhanani94@gmail.com>
2015-09-12 00:57:00 -0400

    express framework is up and running

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var Gun = require('gun');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
+
+var gun = Gun({file: 'data.json'});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function(req, res, next){
+	res.send(200);
+
+});
+
 module.exports = router;
