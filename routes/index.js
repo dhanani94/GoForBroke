9c2286125f9904549ff2379f074e3b01079af8e9
dhanani94 <dhanani94@gmail.com>
2015-09-13 06:14:58 -0400

    basic functionality

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index.html', { title: 'Welcome to my crib.' });
 });
 
+/* GET home page. */
+router.get('/digits', function(req, res, next) {
+  res.render('digits.html', { title: 'Welcome to my crib.' });
+});
+
 module.exports = router;
 
