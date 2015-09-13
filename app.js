3e4bbe7a11e7b51443dc2841cc0c19229f2388de
faizanv <faizan.virani.44@gmail.com>
2015-09-13 01:10:51 -0400

    Changed port settings

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var api = require('./routes/api');
 var Gun = require('gun');
+var port = process.env.PORT || 3000;
 
 
 
